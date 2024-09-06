57df0262ddfaf1496374a93fae98b4df6c39c065
utkarsh98710 <156845520+utkarsh98710@users.noreply.github.com>
2024-09-06 23:04:41 +0530

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,11 @@
+// how to print Hello world in js
+
+// 1 way
+// console.log("Hello world");
+
+// 2 way
+
+let Greet="Hello World";
+
+console.log(Greet);
+
---
+++ 02.js
@@ -0,0 +1,26 @@
+// print number from 1 to 5
+
+console.log("Using for loop");
+
+// for (let i = 1; i <=5; i++) {
+//     console.log(i);
+// }
+
+console.log("Using while loop");
+// let i=1;
+// while (i<=5) {
+//     console.log(i);
+//     i++;
+// }
+
+
+console.log("Using Do while loop");
+
+let i=5;
+do {
+    console.log(i);
+    i++;
+} while (i<=5);
+
+
+
